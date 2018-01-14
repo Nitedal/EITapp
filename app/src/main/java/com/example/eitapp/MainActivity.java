529d096c30629502e0ec244963f3031e54a3cf10
Andreas Wessel Larsen <larsenaw@gmail.com>
2018-01-14 22:26:30 +0100

    second commit

--- app/src/main/java/com/example/eitapp/MainActivity.java
+++ app/src/main/java/com/example/eitapp/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.eitapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        //setContentView(R.layout.activity_main);
+    }
         //a comment for testing
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, infoScreen.class);
+        startActivity(intent);
+
     }
 }
